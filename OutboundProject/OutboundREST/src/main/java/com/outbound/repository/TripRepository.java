0921ba686281e74ee8f71b65c1c656923491bf83
Lpaladini90 <lpaladini@me.com>
2022-06-15 11:21:24 -0700

    trip packages created

---
+++ OutboundProject/OutboundREST/src/main/java/com/outbound/repository/TripRepository.java
@@ -0,0 +1,13 @@
+package com.outbound.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.outbound.entities.Trip;
+
+public interface TripRepository extends JpaRepository<Trip, Integer> {
+
+	
+	
+	
+	
+}
---
+++ OutboundProject/OutboundREST/src/main/java/com/outbound/service/TripService.java
@@ -0,0 +1,13 @@
+package com.outbound.service;
+
+import java.util.List;
+
+import com.outbound.entities.Trip;
+
+public interface TripService {
+
+	public List<Trip> indexAll();
+	
+	
+	
+}
